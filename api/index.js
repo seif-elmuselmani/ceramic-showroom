11ccdfa8d19bfee101bdd2e8d2365b7097237d9e
seif1436 <eldenseif645@gmail.com>
2026-08-01 19:12:36 +0300

    Add Vercel Serverless diagnostic error handling

--- api/index.js
+++ api/index.js
@@ -1,3 +1,19 @@
-const app = require('../server/server.js');
+let app;
+
+try {
+  app = require('../server/server.js');
+} catch (err) {
+  console.error("Vercel Serverless Init Error:", err);
+  const express = require('express');
+  app = express();
+  app.use(express.json());
+  app.all('*', (req, res) => {
+    res.status(500).json({ 
+      error: "Init Error", 
+      message: err.message, 
+      stack: err.stack 
+    });
+  });
+}
 
 module.exports = app;
